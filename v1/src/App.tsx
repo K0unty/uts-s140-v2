469ec314511c4b354952b65a4f946d2c4ec807f7
K0unty <119057948+K0unty@users.noreply.github.com>
2024-06-23 14:27:18 +0000

    .

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   const logoUrl = "https://i.ibb.co/7zgP572/image.png"; // Replace with the URL of the online image
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logoUrl} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
