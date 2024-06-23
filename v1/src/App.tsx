c3629f387923500bee141e0df6c0df4194153ae6
K0unty <119057948+K0unty@users.noreply.github.com>
2024-06-23 14:35:51 +0000

    .

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function App() {
-  const logoUrl = "https://i.ibb.co/7zgP572/image.png"; // Replace with the URL of the online image
+// function App() {
+//   // const logoUrl = "https://i.ibb.co/7zgP572/image.png"; // Replace with the URL of the online image
 
+//   return <div className="App"></div>;
+// }
+
+const App: React.FC = () => {
   return <div className="App"></div>;
-}
+};
 
 export default App;
